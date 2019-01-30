e745be05607eb75bc5e118ee768b823b19837a97
Gabe D <gjdeleon@hotmail.ca>
2019-01-30 16:17:23 -0500

    Added angular components Added routing configuration Added read,update,create ops

---
+++ serverCRUD/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReadComponent } from './operations/read/read.component';
+import { CreateComponent } from './operations/create/create.component';
+import { UpdateComponent } from './operations/update/update.component';
+
+import { MatToolbarModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'create', component: CreateComponent },
+  { path: 'update/:id', component: UpdateComponent },
+  { path: 'read', component: ReadComponent },
+  { path: '', redirectTo: '/read', pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReadComponent,
+    CreateComponent,
+    UpdateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule
+  ],
+  providers: [
+  ],
+  bootstrap: [
+    AppComponent
+  ],
+})
+export class AppModule { }
